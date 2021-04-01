69787de2026f4bbb719c6a062e477ec62cbd4054
zap:Lee-Roy King:zap <lee-roy.king@oracle.com>
2021-04-01 15:26:17 -0700

    it works

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-import flask
+from flask import Flask
+app = Flask(__name__)
 import requests
 import os
 from api_getter import get_stock_info
@@ -12,3 +13,13 @@ helium_data = get_stock_info(COINMARKETAPIKEY)
 helium_price_USD = helium_data["quote"]["USD"]["price"]
 print("and here is your helium price", helium_price_USD)
 
+@app.route("/ed89d968-3acb-4818-ad82-c8fd9f5447e5-c627154e-be84-48f3-af89-7c4878777d4c")
+def helium():
+    COINMARKETAPIKEY = os.environ.get("COINMARKETAPIKEY")
+    helium_data = get_stock_info(COINMARKETAPIKEY)
+    helium_price_USD = helium_data["quote"]["USD"]["price"]
+    helium_price_USD = helium_price_USD * 100 // 1 / 100
+    return str(helium_price_USD)
+
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=True)
